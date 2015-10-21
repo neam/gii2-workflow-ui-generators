6f2833b26018880efdcf66b984da89d34899fd68
Fredrik Wollsén <fredrik@neam.se>
2015-10-22 01:56:21 +0300

    Fixed typo with side-menu/navigation file name structure - part 2

---
+++ angular_crud/default/core/navigation.html.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+use yii\helpers\Html;
+
+$model = $generator->getModel();
+
+$modelClassSingular = get_class($model);
+$modelClassSingularId = Inflector::camel2id($modelClassSingular);
+$modelClassSingularWords = Inflector::camel2words($modelClassSingular);
+$modelClassPluralWords = Inflector::pluralize($modelClassSingularWords);
+$modelClassPlural = Inflector::camelize($modelClassPluralWords);
+$modelClassPluralId = Inflector::camel2id($modelClassPlural);
+$labelSingular = ItemTypes::label($modelClassSingular, 1);
+$labelPlural = ItemTypes::label($modelClassSingular, 2);
+$labelNone = ItemTypes::label($modelClassSingular, 2);
+
+// TODO: handle prefixes through config
+$unprefixedModelClassSingular = str_replace(["Clerk", "Neamtime"], "", get_class($model));
+$unprefixedModelClassSingularId = Inflector::camel2id($unprefixedModelClassSingular);
+$unprefixedModelClassSingularWords = Inflector::camel2words($unprefixedModelClassSingular);
+$unprefixedModelClassPluralWords = Inflector::pluralize($unprefixedModelClassSingularWords);
+$unprefixedModelClassPlural = Inflector::camelize($unprefixedModelClassPluralWords);
+$unprefixedModelClassPluralId = Inflector::camel2id($unprefixedModelClassPlural);
+
+// TODO: fix choiceformat interpretation in yii2 and use item type choiceformat label for labels instead of inflector-created labels
+$labelSingular = $unprefixedModelClassSingularWords;
+$labelPlural = $unprefixedModelClassPluralWords;
+
+?>
+<nav class="navbar-default navbar-static-side animated slideInLeft" role="navigation">
+
+    <ul side-navigation class="nav" id="side-menu" ng-include="'crud/<?= $modelClassSingularId ?>/side-menu.html'" ng-controller="ApiEndpointController">
+
+    </ul>
+
+</nav>
