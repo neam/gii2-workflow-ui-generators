9f4d69b5cf64ebfbd0c7c8208bc95b79c47d5146
Fredrik Wollsén <fredrik@neam.se>
2015-11-26 16:50:12 +0100

    Not showing handsontable if empty collection in curate views

--- angular_crud/default/core/curate.html.php
+++ angular_crud/default/core/curate.html.php
@@ -59,6 +59,7 @@ $labelNone = ItemTypes::label($modelClassSingular, 2);
     <a href="javascript:void(0)" ng-click="<?= lcfirst($modelClassPlural) ?>.add()" ng-show="<?= lcfirst($modelClassPlural) ?>.$promise.$$state.status === 1" class="btn btn-primary btn-xs">Add new item</a>
 
     <simple-handsontable
+        ng-if="<?= lcfirst($modelClassPlural) ?>.length > 0"
         settings="handsontableSettings"
         data="<?= lcfirst($modelClassPlural) ?>">
     </simple-handsontable>
