f062e32174c54332fb531bd8c98942bf64fae283
Fredrik Wollsén <fredrik@neam.se>
2016-10-20 12:46:30 +0300

    Using unprefixed item class labels in loading views

--- angular_crud/default/core/elements/loading-status.html.php
+++ angular_crud/default/core/elements/loading-status.html.php
@@ -14,6 +14,14 @@ $labelPlural = ItemTypes::label($modelClassSingular, 2);
 $labelNone = ItemTypes::label($modelClassSingular, 2);
 // TODO: fix choiceformat interpretation in yii2 and use item type choiceformat label for labels instead of inflector-created labels
 
+// TODO: handle prefixes through config
+$unprefixedModelClassSingular = str_replace(["Clerk", "Neamtime"], "", $generator->modelClass);
+$unprefixedModelClassSingularId = Inflector::camel2id($unprefixedModelClassSingular);
+$unprefixedModelClassSingularWords = Inflector::camel2words($unprefixedModelClassSingular);
+$unprefixedModelClassPluralWords = Inflector::pluralize($unprefixedModelClassSingularWords);
+$unprefixedModelClassPlural = Inflector::camelize($unprefixedModelClassPluralWords);
+$unprefixedModelClassPluralId = Inflector::camel2id($unprefixedModelClassPlural);
+
 ?>
 
 <div class="alert alert-info" ng-show="!<?= lcfirst($modelClassPlural) ?>.$refreshing && <?= lcfirst($modelClassPlural) ?>.$resolved === null">
@@ -21,21 +29,21 @@ $labelNone = ItemTypes::label($modelClassSingular, 2);
 </div>
 
 <div class="alert alert-info" ng-show="<?= lcfirst($modelClassPlural) ?>.$refreshing">
-    Loading <?= strtolower($modelClassPluralWords) ?>...
+    Loading <?= strtolower($unprefixedModelClassPluralWords) ?>...
 </div>
 
 <div class="alert alert-warning"
      ng-show="<?= lcfirst($modelClassPlural) ?>.$promise.$$state.status === 1 && <?= lcfirst($modelClassPlural) ?>.length == 0">
     <div ng-show="!<?= lcfirst($modelClassPlural) ?>.filtered()">
-        You have no <?= strtolower($modelClassPluralWords) ?>.
+        You have no <?= strtolower($unprefixedModelClassPluralWords) ?>.
     </div>
     <div ng-show="<?= lcfirst($modelClassPlural) ?>.filtered()">
-        No <?= strtolower($modelClassPluralWords) ?> matched the current filters.
+        No <?= strtolower($unprefixedModelClassPluralWords) ?> found to display.
     </div>
 </div>
 
 <div class="alert alert-danger"
      ng-show="<?= lcfirst($modelClassPlural) ?>.$promise.$$state.status === 2">
-    A problem was encountered when loading the <?= strtolower($modelClassPluralWords) ?>.
+    A problem was encountered when loading the <?= strtolower($unprefixedModelClassPluralWords) ?>.
     <a href="javascript:void(0)" ng-click="<?= lcfirst($modelClassPlural) ?>.refresh()" class="btn btn-primary btn-xs">Click here to retry</a>
 </div>
