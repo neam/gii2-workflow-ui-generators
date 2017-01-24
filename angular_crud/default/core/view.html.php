eeae0cc5c73cc1e2bb97b386756439894d744d0f
Fredrik Wollsén <fredrik@neam.se>
2017-01-24 13:43:19 +0200

    Added empty view.html

---
+++ angular_crud/default/core/view.html.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+use yii\helpers\Html;
+
+$model = $generator->getModel();
+
+$modelClassSingular = $generator->modelClass;
+$modelClassSingularId = Inflector::camel2id($modelClassSingular);
+$modelClassSingularWords = Inflector::camel2words($modelClassSingular);
+$modelClassPluralWords = Inflector::pluralize($modelClassSingularWords);
+$modelClassPlural = Inflector::camelize($modelClassPluralWords);
+$modelClassPluralId = Inflector::camel2id($modelClassPlural);
+$labelSingular = ItemTypes::label($modelClassSingular, 1);
+$labelPlural = ItemTypes::label($modelClassSingular, 2);
+$labelNone = ItemTypes::label($modelClassSingular, 2);
+
+// TODO: handle prefixes through config
+$unprefixedModelClassSingular = str_replace(["Clerk", "Neamtime"], "", $generator->modelClass);
+$unprefixedModelClassSingularId = Inflector::camel2id($unprefixedModelClassSingular);
+$unprefixedModelClassSingularWords = Inflector::camel2words($unprefixedModelClassSingular);
+$unprefixedModelClassPluralWords = Inflector::pluralize($unprefixedModelClassSingularWords);
+$unprefixedModelClassPlural = Inflector::camelize($unprefixedModelClassPluralWords);
+$unprefixedModelClassPluralId = Inflector::camel2id($unprefixedModelClassPlural);
+
+// TODO: fix choiceformat interpretation in yii2 and use item type choiceformat label for labels instead of inflector-created labels
+$labelSingular = ucfirst(strtolower($unprefixedModelClassSingularWords));
+$labelPlural = ucfirst(strtolower($unprefixedModelClassPluralWords));
+
+?>
+
+<pre>{{ item | json }}</pre>
